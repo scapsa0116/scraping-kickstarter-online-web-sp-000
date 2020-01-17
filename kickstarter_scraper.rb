6c14256e3fb6adfa20ead86f6878754034e00496
Stela Capsa <stela.capsa@gmail.com>
2020-01-17 19:37:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
 end
  
 create_project_hash
