8feb9867cdd6fb9ad74e1307aecb7343a5ba6c7d
Stela Capsa <stela.capsa@gmail.com>
2020-01-17 19:29:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,10 @@
+require 'nokogirl'
+
 # require libraries/modules here
 
+
 def create_project_hash
-  # write your code here
+ html = File.read('fixtures/kickstarter.html')
+ 
+kickstarter = Nokogiri::HTML(html)
 end
